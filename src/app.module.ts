f34747db7f7cc75cf8775307f7bcb3e597a74358
zoek <felipe.esbu@gmail.com>
2021-06-23 17:54:07 -0400

    ✨ adding app module with typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { WeatherModule } from './weather/weather.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'weather_status',
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+    WeatherModule
+  ],
   controllers: [],
   providers: [],
 })
