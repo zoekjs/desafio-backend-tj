766937291a804a0a904b2d4178d1b3484bcf82ea
zoek <felipe.esbu@gmail.com>
2021-06-23 17:52:26 -0400

    ✨ adding service for weather API

---
+++ src/weather/services/weather.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
+import { WeatherStatus } from '../entities/weatherStatus.entity';
+import { ReadWeatherStatusDto } from '../dto/ReadWeatherStatusDTO.dto';
+import { HttpRequestService } from './http-request.service';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class WeatherService {
+
+  constructor(@InjectRepository(WeatherStatus) private weatherStatusRepository: Repository < WeatherStatus > ,
+    private httpRequestService: HttpRequestService) {}
+
+  private citiesWeatherStatus: ReadWeatherStatusDto[] = [];
+
+  async getCitiesWeatherStatus(cities: string[]): Promise < ReadWeatherStatusDto[] > {
+    return Promise.all(cities.map(async city => {
+      const existedWeatherStatus = await this.getWeatherStatus(city);
+      const weatherStatus: ReadWeatherStatusDto = (existedWeatherStatus)
+        ? existedWeatherStatus
+        : await this.createWeatherStatus(city)
+      return weatherStatus;
+    }))
+  }
+
+  async createWeatherStatus(city: string): Promise < ReadWeatherStatusDto > {
+    const weatherStatus = await this.httpRequestService.getWeatherStatusFromAPI(city)
+    const savedData = await this.weatherStatusRepository.save(this.weatherStatusRepository.create(weatherStatus))
+    return plainToClass(ReadWeatherStatusDto, savedData)
+  }
+
+  async getWeatherStatus(city: string): Promise < ReadWeatherStatusDto > {
+    const search = await this.weatherStatusRepository.findOne({
+      where: {
+        city: this.capitalizeFirstLetter(city)
+      },
+      order: {
+        created_at: 'DESC'
+      }
+    })
+    return plainToClass(ReadWeatherStatusDto, search)
+  }
+
+  capitalizeFirstLetter(string: string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+}
