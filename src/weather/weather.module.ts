83775fd8771a914a35615f2e23229ad9db3f04ad
zoek <felipe.esbu@gmail.com>
2021-06-23 17:53:14 -0400

    ✨ adding module with middleware response time implementation

---
+++ src/weather/weather.module.ts
@@ -0,0 +1,24 @@
+import { HttpModule, MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { WeatherController } from './controllers/weather.controller';
+import { WeatherService } from './services/weather.service';
+import { HttpRequestService } from './services/http-request.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WeatherStatus } from './entities/weatherStatus.entity';
+import { ResponseTimeMiddleware } from '@nest-middlewares/response-time';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([WeatherStatus]),
+    HttpModule
+  ],
+  controllers: [WeatherController],
+  providers: [WeatherService, HttpRequestService]
+})
+export class WeatherModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ResponseTimeMiddleware).forRoutes(
+      { path: 'api/weather/*', method: RequestMethod.GET}, 
+      { path: 'api/weather', method: RequestMethod.POST}
+    )
+  }
+}
