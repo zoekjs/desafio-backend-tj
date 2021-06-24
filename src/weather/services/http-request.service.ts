75f4c29f5edf83eb05ea30b84a41e5660a4554fd
zoek <felipe.esbu@gmail.com>
2021-06-24 16:46:54 -0400

    ✨ added variables to url settings

--- src/weather/services/http-request.service.ts
+++ src/weather/services/http-request.service.ts
@@ -1,6 +1,7 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { map } from 'rxjs/operators';
 import { WeatherStatus } from '../entities/weatherStatus.entity';
+import { apiKey, units } from '../../../config';
 
 @Injectable()
 export class HttpRequestService {
@@ -8,7 +9,7 @@ export class HttpRequestService {
   constructor(private httpService: HttpService) {}
 
   async getWeatherStatusFromAPI(city: string): Promise<WeatherStatus> {
-    const data = await this.httpService.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=42e933916e075bec2020bb8d56356c15`,
+    const data = await this.httpService.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`,
         { headers: { 'Accept': 'application/json' } })
         .pipe(
           map(response => response.data)
