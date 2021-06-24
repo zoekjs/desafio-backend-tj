4977aeb79dd19be9bc475b5fb37c015cde13d39a
zoek <felipe.esbu@gmail.com>
2021-06-24 16:42:16 -0400

    ✨ adding validation pipes config - swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const options = new DocumentBuilder()
+    .setTitle('Weather API')
+    .setDescription('API de consulta de clima (desafío backend TJ)')
+    .setVersion('1.0')
+    .addTag('Weather')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions:{
+      showRequestDuration:true
+    }
+  })
   await app.listen(3000);
 }
 bootstrap();
